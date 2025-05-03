28ad4d2ea341a31f26fc0834228a39519b539a82
movic17 <69178985+movic17@users.noreply.github.com>
2025-05-03 12:19:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Route, BrowserRouter as router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import ProxyPage from "./pages/ProxyPage";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <Home />;
+  return (
+    <router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="#/:id" element={<ProxyPage />} />
+        <Route path="#/*" element={<NotFound />} />
+      </Routes>
+    </router>
+  );
 }
 
 export default App;
